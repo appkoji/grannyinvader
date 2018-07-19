19853c204e893e732c993bc9af8890b4b722e4ea
Koji Murata <muratapp@gmail.com>
2018-07-19 21:29:19 +0800

    Initial Commit

---
+++ ARTreasure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARTreasure
+//
+//  Created by Koji Murata on 19/07/2018.
+//  Copyright © 2018 KojiGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARTreasure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARTreasure
+//
+//  Created by Koji Murata on 19/07/2018.
+//  Copyright © 2018 KojiGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
